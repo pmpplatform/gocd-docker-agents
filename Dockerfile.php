7a28ca6a2a1c8d7cc014bf8f68fb82227e538295
Chad Jones <cj@patientsky.com>
2024-07-04 10:31:29 +0200

    Add php agent

---
+++ Dockerfile.php
@@ -0,0 +1,29 @@
+FROM gocd/gocd-agent-debian-11:v23.1.0
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+USER root
+
+# Base packages
+RUN apt-get update && \
+  apt-get install -y --no-install-recommends \
+  curl \
+  wget \
+  gnupg \
+  ca-certificates \
+  make \
+  lsb-base \
+  lsb-release
+
+# php repo
+RUN echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/sury-php.list
+RUN wget -qO - https://packages.sury.org/php/apt.gpg | apt-key add -
+
+RUN apt-get update && \
+  apt-get install -y --no-install-recommends \
+  php8.2 \
+  php8.2-cli \
+  php8.2-common
+
+# Install Composer
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
